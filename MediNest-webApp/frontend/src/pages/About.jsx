7a24e0d72daa41254463249524e7d2cb5c9b85fa
Tushar Pathare <patharetushar10@gmail.com>
2025-08-30 00:22:09 +0530

    Update pages and components: TopDoctors, Appointments, About, Contact, Login, MyAppointments, MyProfile, RelatedDoctors

--- MediNest-webApp/frontend/src/pages/About.jsx
+++ MediNest-webApp/frontend/src/pages/About.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ABOUT <span className="text-gray-700 font-medium">US</span>
+        </p>
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img
+          src={assets.about_image}
+          className="w-full md:max-w-[360px]"
+          alt="about_image"
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+          <p>
+            Welcome to MediNest, your reliable partner in managing healthcare
+            appointments and medical records efficiently. MediNest makes it
+            simple for patients to book appointments, track their health, and
+            stay connected with healthcare providers, all through one intuitive
+            platform.
+          </p>
+          <p>
+            With cutting-edge technology and a focus on user experience,
+            MediNest ensures that both patients and clinics enjoy smooth,
+            hassle-free healthcare management — whether it’s scheduling your
+            first visit or managing ongoing treatments.
+          </p>
+          <b className="text-gray-800">Our Vision</b>
+          <p>
+            At MediNest, our vision is to create a seamless and accessible
+            healthcare experience for everyone. We aim to bridge the gap between
+            patients and healthcare providers, making quality care available
+            when and where it’s needed most.
+          </p>
+        </div>
+      </div>
+
+      <div className="text-xl my-4">
+        <p className="text-gray-400">
+          WHY <span className="text-gray-700 font-medium">CHOOSE US</span>
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Efficiency:</b>
+          <p>
+            Streamlined appointment scheduling that fits into your busy
+            lifestyle.
+          </p>
+        </div>
+
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Convenience:</b>
+          <p>
+            Access to a network of trusted healthcare professionals in your
+            area.
+          </p>
+        </div>
+
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Personalization:</b>
+          <p>
+            Tailored recommendations and reminders to help you stay on top of
+            your health.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- MediNest-webApp/frontend/src/pages/Appointments.jsx
+++ MediNest-webApp/frontend/src/pages/Appointments.jsx
@@ -2,30 +2,183 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointments = () => {
   const [docInfo, setDocInfo] = useState(null);
+  const [doctSlots, setDoctSlots] = useState([]);
+  const [slotIndex, seSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
+
+  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   const { docId } = useParams();
-  const { doctors } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find((doc) => doc._id == docId);
     setDocInfo(docInfo);
   };
 
+  const getAvailableSlots = async () => {
+    setDoctSlots([]);
+
+    // getting current data
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      // getting data with index
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+
+      // setting end time of the date with index
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+
+      //setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+      let timeSlots = [];
+
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+
+        // add slots to array
+        timeSlots.push({
+          dataTime: new Date(currentDate),
+          time: formattedTime,
+        });
+
+        // Increment current time by 30 min
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+
+      setDoctSlots((prev) => [...prev, timeSlots]);
+    }
+  };
+
   useEffect(() => {
     fetchDocInfo();
   }, [doctors, docId]);
 
-  return (
-  <div>
-    {/* -------Doctor Deatils------ */}
-    <div>
-      <div>
+  useEffect(() => {
+    getAvailableSlots();
+  }, [docInfo]);
 
+  useEffect(() => {
+    console.log(doctSlots);
+  }, [doctSlots]);
+
+  return (
+    docInfo && (
+      <div>
+        {/* -------Doctor Deatils------ */}
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div>
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt="doctorImage"
+            />
+          </div>
+
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+            {/* ------------- Doc Info : name , degree , experience  --------- */}
+
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{" "}
+              <img
+                className="w-5"
+                src={assets.verified_icon}
+                alt="verified_icon"
+              />
+            </p>
+
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </div>
+
+            {/* -------Doctor About------- */}
+
+            <div>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="info_icon" />
+              </p>
+              <p className="text-sm text-gray-500 max-w-[700px] mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment fee:
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
+            </p>
+          </div>
+        </div>
+
+        {/* ---------- Booking Slots  ---------*/}
+        <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
+          <p>Booking slots</p>
+          <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+            {doctSlots.length &&
+              doctSlots.map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => seSlotIndex(index)}
+                  className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${
+                    slotIndex === index
+                      ? "bg-primary text-white"
+                      : "border border-gray-200 "
+                  }`}
+                >
+                  <p>{item[0] && daysOfWeek[item[0].dataTime.getDay()]}</p>
+                  <p>{item[0] && item[0].dataTime.getDate()}</p>
+                </div>
+              ))}
+          </div>
+          <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
+            {doctSlots.length &&
+              doctSlots[slotIndex].map((item, index) => (
+                <p
+                  onClick={() => setSlotTime(item.time)}
+                  key={index}
+                  className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${
+                    item.time === slotTime
+                      ? "bg-primary text-white"
+                      : "text-gray-400 border border-gray-300"
+                  }`}
+                >
+                  {item.time.toLowerCase()}
+                </p>
+              ))}
+          </div>
+          <button className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6">
+            Book an appointment
+          </button>
+        </div>
+
+        {/* ------- Listing Related Doctors ----- */}
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
       </div>
-    </div>
-  </div>
+    )
   );
 };
 
